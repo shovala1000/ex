8316c38c2826e788ac15be699e1b6b8aa70a0198
shoval <shovala1000@gmail.com>
2021-10-18 12:54:07 +0300

    ex.h1

--- main.cpp
+++ main.cpp
@@ -4,5 +4,7 @@ int main() {
     std::cout << "Hello, World!" << std::endl;
     std:: cout << "Hi shoval" << std::endl;
     std:: cout << "Hi shoval" << std::endl;
+    std:: cout << "Hi sagi" << std::endl;
     return 0;
+    //added hi sagi
 }
